e16eb0b8d56fe9c64a68d758cdcbd7a294e2818c
AliceKowai <devalice117@gmail.com>
2023-10-18 16:31:31 -0300

    Adicionando a area do quadrado e desafio03

---
+++ AV/aula10.18/area_do_quadrado.py
@@ -0,0 +1,5 @@
+base = input("Informa a base do quadrado: ")
+altura = input("Informa a altura do quadrado: ")
+
+calculo_da_area = int(base) * int(altura)
+print("A area do quadrado é: \n", calculo_da_area)
---
+++ AV/aula10.18/desafio03.py
@@ -0,0 +1,5 @@
+nota1 = input("Digite primeira nota: ")
+nota2 = input("Digite segunda nota: ")
+media = (int(nota1) + int(nota2) )/ 2
+
+print("A média é: ", media)
