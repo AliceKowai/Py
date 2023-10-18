57d88c132a287d98b6bd6740ccc7b946deb5bd2f
AliceKowai <devalice117@gmail.com>
2023-10-18 14:30:14 -0300

    não nomeados adicionado.

---
+++ AV/aula10.17/sep.py
@@ -0,0 +1,4 @@
+pessoa = "Alice"
+
+print("Alice", "está", "com", "sono", sep="**", end="\n")
+print(type(pessoa))
---
+++ AV/aula10.18/nao_nomeados.py
@@ -0,0 +1,8 @@
+#valores não nomeados
+a= 1
+b= 1
+soma = a + b
+print(soma) #valores nomeados
+print (1 + 1) #valores não nomeados
+
+#valores não nomeados não podem se estender no código. Você não pode invocar no código.
