7a66591f8e595c3806130ff04b9a3b46fa266093
AliceKowai <devalice117@gmail.com>
2023-10-18 15:55:10 -0300

    desafio02 adicionado

---
+++ AV/aula10.18/desafio02.py
@@ -0,0 +1,6 @@
+idade = int(input("Digite sua idade aqui: "))
+idade_parente = int(input("Digite a idade de um parente: "))
+nome = input("Digite seu nome aqui: ")
+
+print(idade * idade_parente)
+print(nome)
