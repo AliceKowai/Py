08d5150e60577435b0881c318b5c63dabfefa812
AliceKowai <devalice117@gmail.com>
2023-10-19 15:41:23 -0300

    Foi adicionado os operadores aritmeticos e tipos2

---
+++ AV/aula10.19/operadores_aritmeticos.py
@@ -0,0 +1,9 @@
+#operações aritmeticas
+
+a = 3 + 3
+b = 5 - 2
+c = 4 * 4
+d = 8 / 2
+e = 3 % 2
+f = 3 **2
+print(e , f)
--- AV/aula10.19/tipos02.py
+++ AV/aula10.19/tipos02.py
@@ -1,1 +1,9 @@
 # Tipos e mudanças de tipos 
+a = 1.2
+b = "1"
+print(type(a))
+print(type(b))
+
+c = int(a)
+print(type(c))
+
