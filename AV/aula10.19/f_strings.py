886a713d3345ac910e9a777332155cdd1fc2a1fe
AliceKowai <devalice117@gmail.com>
2023-10-19 15:15:31 -0300

    Adicionado o f_string - interpolação

---
+++ AV/aula10.19/f_strings.py
@@ -0,0 +1,7 @@
+# Interpolando strings com codigo python
+nome = input("Qual é seu nome? ")
+#concatenação
+print("Seja muito bem-vindo,", nome)
+
+#Interpolação
+print(f'Seja muito bem-vindo, {nome}. Estavámos esperando por você!!!')
